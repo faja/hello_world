9bb1a84aea6147d17e969a7ae6bf015154072299
faja <mcabaj@gmail.com>
2018-04-23 09:37:40 +0100

    added /loadavg endpoint

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -10,7 +11,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello world\n")
 }
 
+func handlerLoad(w http.ResponseWriter, r *http.Request) {
+	data, _ := ioutil.ReadFile("/proc/loadavg")
+	fmt.Fprint(w, string(data))
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/loadavg", handlerLoad)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
